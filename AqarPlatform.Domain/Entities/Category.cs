4c310290761c23dd0b1b8bd175efa6b4d3f2ea62
mohammedzaghloul <145876544+mohammedzaghloul@users.noreply.github.com>
2026-07-09 08:48:58 +0300

    feat(domain => category): add description to category

--- AqarPlatform.Domain/Entities/Category.cs
+++ AqarPlatform.Domain/Entities/Category.cs
@@ -5,6 +5,7 @@ namespace AqarPlatform.Domain.Entities
     public class Category : BaseEntity
     {
         public string Name { get; set; } = string.Empty;
+        public string Description { get; set; }
 
         public ICollection<Property> Properties { get; set; } = [];
     }
---
+++ AqarPlatform.Persistence/Configurations/CategoryConfigration/CategoryConfg.cs
@@ -0,0 +1,25 @@
+﻿using AqarPlatform.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AqarPlatform.Persistence.Configurations.CategoryConfigration
+{
+    public class CategoryConfg : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Category");
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Name)
+                   .IsRequired()
+                   .HasMaxLength(100);
+            builder.Property(c => c.)
+            .IsRequired()
+            .HasMaxLength(100);
+        }
+    }
+}

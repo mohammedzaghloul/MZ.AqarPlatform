8959c73e5e07a7cdcaf84d13858410b5a041480f
mohammedzaghloul <145876544+mohammedzaghloul@users.noreply.github.com>
2026-07-09 08:59:12 +0300

    feat(Configuration): add category entity configuration

--- AqarPlatform.Persistence/Configurations/CategoryConfigration/CategoryConfg.cs
+++ AqarPlatform.Persistence/Configurations/CategoryConfigration/CategoryConfg.cs
@@ -13,13 +13,22 @@ namespace AqarPlatform.Persistence.Configurations.CategoryConfigration
         public void Configure(EntityTypeBuilder<Category> builder)
         {
             builder.ToTable("Category");
+
             builder.HasKey(c => c.Id);
+
             builder.Property(c => c.Name)
                    .IsRequired()
                    .HasMaxLength(100);
-            builder.Property(c => c.)
+
+            builder.Property(c => c.Description)
             .IsRequired()
             .HasMaxLength(100);
+
+            builder.HasMany(c=> c.Properties)
+                .WithOne(p=> p.Category)
+                .HasForeignKey(p => p.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
         }
     }
 }

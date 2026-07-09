dfaeefd57f860e24932297fd3c75873163ae70d1
mohammedzaghloul <145876544+mohammedzaghloul@users.noreply.github.com>
2026-07-09 09:14:17 +0300

    feat(Configuration): add city entity configuration

---
+++ AqarPlatform.Persistence/Configurations/CityConfigration/CityConfg.cs
@@ -0,0 +1,24 @@
+﻿using AqarPlatform.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AqarPlatform.Persistence.Configurations.CityConfigration
+{
+    public class CityConfg : IEntityTypeConfiguration<City>
+    {
+        public void Configure(EntityTypeBuilder<City> builder)
+        {
+            builder.Property(c=>c.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.HasMany(c => c.Properties)
+                   .WithOne(p => p.City)
+                   .HasForeignKey(p => p.CityId)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}

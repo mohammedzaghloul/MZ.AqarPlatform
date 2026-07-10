82950f212bed6f26292c5328dd4e441e1d79055a
mohammedzaghloul <145876544+mohammedzaghloul@users.noreply.github.com>
2026-07-10 14:45:07 +0300

    feta (Persistence) : add favorite entity configuration

---
+++ AqarPlatform.Persistence/Configurations/FavoriteConfigurations/FavoriteConfg.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AqarPlatform.Persistence.Configurations.FavoriteConfigurations
+{
+    internal class FavoriteConfg
+    {
+    }
+}

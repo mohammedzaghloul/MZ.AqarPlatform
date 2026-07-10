bfd8e9822b72b71a84c9ddd9f0e2c87590e8b406
mohammedzaghloul <145876544+mohammedzaghloul@users.noreply.github.com>
2026-07-10 20:03:51 +0300

    feta (Persistence) add UnitOfWork and  GenericRepository

---
+++ AqarPlatform.Application/Interfaces/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using AqarPlatform.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AqarPlatform.Domain.Interfaces
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T?> GetByIdAsync(Guid id);
+        Task<IEnumerable<T>> GetAllAsync();
+
+       public Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+        Task SaveChangesAsync();
+    }
+}
---
+++ AqarPlatform.Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using AqarPlatform.Domain.Common;
+using AqarPlatform.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AqarPlatform.Application.Interfaces
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        public IGenericRepository<T> Repository<T>() where T : BaseEntity;
+    }
+}
